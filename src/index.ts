3489ab94199927fb0581aad7f3b084e3f4b3945e
Fynn Haupt <fynn.haupt@fynnhaupt.de>
2023-06-03 12:25:07 +0200

    export all types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 export {ClientClient, ClientUpdateDataT} from "./classes/ClientClient";
 export {ServerClient, ServerUpdateDataT} from "./classes/ServerClient";
+
+export {EventT} from "krp-node-wrapper/dist/types/EventT";
+export {EntryT} from "krp-node-wrapper/dist/types/EntryT";
+export {EntryRemoveT} from "krp-node-wrapper/dist/types/EntryRemoveT";
+export {SessionT} from "krp-node-wrapper/dist/types/SessionT";
+export {SessionStatusT} from "krp-node-wrapper/dist/types/SessionStatusT";
+export {WeatherT} from "krp-node-wrapper/dist/types/WeatherT";
+export {SessionEntryT} from "krp-node-wrapper/dist/types/SessionEntryT";
+export {DriverStatusT} from "krp-node-wrapper/dist/types/DriverStatusT";
+export {BestLapT} from "krp-node-wrapper/dist/types/BestLapT";
+export {LastLapT} from "krp-node-wrapper/dist/types/LastLapT";
+export {PenaltyT} from "krp-node-wrapper/dist/types/PenaltyT";
+export {LapT} from "krp-node-wrapper/dist/types/LapT";
+export {SplitT} from "krp-node-wrapper/dist/types/SplitT";
+export {SpeedT} from "krp-node-wrapper/dist/types/SpeedT";
+export {ClassificationT} from "krp-node-wrapper/dist/types/ClassificationT";
+export {ChallengeDataT} from "krp-node-wrapper/dist/types/ChallengeDataT";
+export {TrackDataT} from "krp-node-wrapper/dist/types/TrackDataT";
+export {TrackSegmentT} from "krp-node-wrapper/dist/types/TrackSegmentT";
+export {TrackPositionT} from "krp-node-wrapper/dist/types/TrackPositionT";
+export {ContactT} from "krp-node-wrapper/dist/types/ContactT";
+
+export {KartDataT} from "krp-node-wrapper/dist/types/KartDataT";
+export {KartEventT} from "krp-node-wrapper/dist/types/KartEventT";
+export {KartLapT} from "krp-node-wrapper/dist/types/KartLapT";
+export {KartSessionT} from "krp-node-wrapper/dist/types/KartSessionT";
+export {KartSplitT} from "krp-node-wrapper/dist/types/KartSplitT";
